fed6f3512a8d40739e3f2b28c8578af601ba99e1
Lucas Martins <foxeytuby2022@gmail.com>
2026-04-02 10:28:16 +0100

    -fazer o formulário -colocar a data e descrição -remover funcionalidade de adicionar colunas

--- app/composables/useFormatter.ts
+++ app/composables/useFormatter.ts
@@ -1,15 +1,15 @@
 export const useFormatter = () => {
-  const formatDate = (dateInput: string | Date): string => {
+  const formatDate = (dateInput: any): string => {
     const date = new Date(dateInput)
 
-    return new Intl.DateTimeFormat('pt-PT', {
+    if (!dateInput || isNaN(date.getTime())) return 'Sem data'
+
+    return date.toLocaleDateString('pt-PT', {
       day: '2-digit',
       month: 'long',
       year: 'numeric'
-    }).format(date)
+    })
   }
 
-  return {
-    formatDate
-  }
+  return { formatDate }
 }
