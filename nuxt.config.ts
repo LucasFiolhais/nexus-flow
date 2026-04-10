78f5e132d0e5c2a88af62f4450c7ee8d96eea8c2
Lucas Martins <foxeytuby2022@gmail.com>
2026-04-10 13:40:12 +0100

    criar versão mobile

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,30 @@
 export default defineNuxtConfig({
   ssr: false,
+
   devServer: {
-    host: '127.0.0.1',
+    host: '0.0.0.0',
     port: 3000
   },
+
+  nitro: {
+    preset: 'service-worker'
+  },
+
   vite: {
     server: {
       watch: {
         usePolling: true
       },
-      hmr: false
+      hmr: {
+        protocol: 'ws',
+        host: '0.0.0.0'
+      }
     }
   },
+
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+
   modules: [
     '@pinia/nuxt',
     '@pinia-plugin-persistedstate/nuxt',
